484ab1cd3f9d403bc2e4e71ab2667d52e5af42df
yebin Park <yebinp1102@naver.com>
2023-11-28 23:46:08 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css';
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div className="text-3xl font-bold underline">
+      App
+    </div>
+  )
+}
+
+export default App
