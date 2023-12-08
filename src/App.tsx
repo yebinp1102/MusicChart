f9f10c52cf8517bc038ea8902e3ba458c5c1b384
yebin Park <yebinp1102@naver.com>
2023-12-08 21:24:29 +0900

    Feat: image file uploader UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AuthLayout from './_auth/AuthLayout';
 import LoginFrom from './_auth/form/LoginFrom';
 import RegisterForm from './_auth/form/RegisterForm';
 import RootLayout from './_root/RootLayout';
-import { Chart, Explore, Genre, Home, LikedList, RecentPlayed, Playlist, Profile } from './_root/pages';
+import { Chart, Explore, Genre, Home, LikedList, RecentPlayed, Playlist, Profile, CreateSong } from './_root/pages';
 import { Toaster } from './components/ui/toaster';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
@@ -27,6 +27,7 @@ const App = () => {
           <Route index path='/playlist' element={<Playlist />} />
           <Route index path='/profile/:id/*' element={<Profile />} />
           <Route index path='/list-recently-played' element={<RecentPlayed />} />
+          <Route index path='/create-post' element={<CreateSong />} />
         </Route>
 
       </Routes>
--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -6,3 +6,4 @@ export {default as LikedList} from "./LikedList";
 export {default as Playlist} from "./Playlist";
 export {default as Profile} from "./Profile";
 export {default as RecentPlayed} from "./RecentPlayed";
+export {default as CreateSong} from "./CreateSong";
